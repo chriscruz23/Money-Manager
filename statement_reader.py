f4c476287ee8943c55e372a2c6ccae8fb5e34ce6
chriscruz23 <62638116+chriscruz23@users.noreply.github.com>
2023-07-04 00:24:28 -0400

    statement reader now takes into account negative amounts and main now reflects all calculations.

--- statement_reader.py
+++ statement_reader.py
@@ -1,23 +1,57 @@
 """Modules used to read statement data and pre-process the transactions.
 """
 
+import re
 from os import scandir
-from re import Pattern, findall
 
 from PyPDF2 import PdfReader
 
 
-def _get_statement_transactions(file_name: str, regex: Pattern) -> list[str]:
+def _read__statement_text(file_name: str):
     text = ""
     with open(file_name, "rb") as file:
+        year = file_name.name.split("-")[0][-4:]
         reader = PdfReader(file)
         for page in reader.pages:
             text += page.extract_text()
 
-    return findall(regex, text)
+    return year, text
 
 
-def get_all_transactions(folder_path: str, regex: Pattern) -> list[str]:
+def get_statement_transactions(
+    file_name: str, transactions_regex: re.Pattern, withdrawals_start_regex: re.Pattern
+) -> list[str]:
+    """Given a specific bank statement pdf, search and return transaction data matching the given
+    regex.
+
+    Args:
+        file_name (str): path to a bank statement pdf file containing transaction data.
+        regex (Pattern): regex pattern used to find all transaction data in the given statement.
+
+    Returns:
+        list[str]: a list of all matched transactions of type String for this file.
+    """
+
+    year, raw_text = _read__statement_text(file_name)
+    matches = []
+
+    withdrawals_start = None
+    if re.search(withdrawals_start_regex, raw_text):
+        withdrawals_start = re.search(withdrawals_start_regex, raw_text).end()
+
+    for match in re.finditer(transactions_regex, raw_text):
+        sign = " +"
+        if withdrawals_start and match.start() > withdrawals_start:
+            sign = " -"
+        match = sign.join(match.group().split(" ", 1))
+        matches.append(f"{year}/{match}")
+
+    return matches
+
+
+def get_all_transactions_from_folder(
+    folder_path: str, regex: re.Pattern, withdrawals_start_regex: re.Pattern
+) -> list[str]:
     """Given a folder path and an accompanying regex pattern to match the desired transactions,
     read each statement pdf and return a list of transactions ready for pre-processing.
 
@@ -26,10 +60,12 @@ def get_all_transactions(folder_path: str, regex: Pattern) -> list[str]:
         regex (re.Pattern): regex pattern used to find all transaction data in the given statements.
 
     Returns:
-        list[str]: a list of all matched transactions of type String.
+        list[str]: a list of all matched transactions of type String for all files in this folder.
     """
     raw_data = []
     for statement in scandir(folder_path):
-        raw_data += _get_statement_transactions(statement, regex)
+        raw_data += get_statement_transactions(
+            statement, regex, withdrawals_start_regex
+        )
 
     return raw_data
