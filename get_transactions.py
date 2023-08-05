ddb32169aa750b0bf186041359bda5a6e5f705ce
chriscruz23 <62638116+chriscruz23@users.noreply.github.com>
2023-08-04 22:51:29 -0400

    Completed reading Discover pdf files.

--- get_transactions.py
+++ get_transactions.py
@@ -1,81 +1,78 @@
 import multiprocessing as mp
-import re
-from calendar import monthrange
+from calendar import month_abbr, monthrange
 from os import path, walk
 
+import regex as re
 from PyPDF2 import PdfReader
 
-NEW_PDFS = r"/data/01_raw/pdfs/Discover/tester"
 NEW_REGEX = re.compile(r"\d{1,2}/\d{2}.*?\n?.*-?\$\d+\.\d{2}", re.MULTILINE)
-MONTHS = {
-    "Jan": 1,
-    "Feb": 2,
-    "Mar": 3,
-    "Apr": 4,
-    "May": 5,
-    "Jun": 6,
-    "Jul": 7,
-    "Aug": 8,
-    "Sep": 9,
-    "Oct": 10,
-    "Nov": 11,
-    "Dec": 12,
-}
+MONTH_NUM = {m: i for i, m in enumerate(month_abbr) if m}  # eg. ("Jan": 1)
+NUM_MONTH = {i: m for i, m in enumerate(month_abbr) if m}  # eg. (1: "Jan")
 
 
 def _get_file_paths(folder: str) -> list[str]:
     f = []
-    for root, _, fns in walk(folder):
-        for file in fns:
-            f.append(path.join(root, file))
+    for root, _, file_names in walk(folder):
+        [f.append(path.join(root, fn)) for fn in file_names]
     return f
 
 
 def _get_single_statement_text(file: str) -> str:
-    t = ""
+    text = ""
     with open(file, "rb") as f:
         r = PdfReader(f)
         for page in r.pages:
-            t += page.extract_text()
-    return t
+            text += page.extract_text().replace("\n", " ")
+    return text
 
 
 def get_all_statement_text(folder: str) -> list[str]:
     # mutiprocessing map to read all pdfs asynchronously
-    p = mp.Pool()
-    t = p.map(_get_single_statement_text, _get_file_paths(folder))
-    p.close()
-    p.join()
-
-    return t
+    with mp.Pool() as pool:
+        return pool.map(_get_single_statement_text, _get_file_paths(folder))
 
 
-def process_old_discover_transactions(folder: str) -> list[str]:
-    """Given a folder directory of old format Discover pdf statements, return all transactions found.
+def process_old_discover_statements(folder: str) -> list[str]:
+    DATE_REG = re.compile(r"(?<=Close Date: )(\w{3}) \d{1,2}, (\d{4})")
+    REG = re.compile(
+        r"(?<=\b\w{3}\b \d{1,2} )\b\w{3}\b \d{1,2} [\s\w\-\/\*#]* \$? ?-?\d?,?\d+\.\d{2}|INTEREST CHARGE ON PURCHASES \$? -?\d?,?\d+\.\d{2}"
+    )
 
-    Args:
-        folder (str): path to the old format Discover pdf file.
-
-    Returns:
-        list[str]: all transactions found within the pdf files in the given directory.
-    """
-    REG = r"\w{3} \d{1,2} (\w{3} \d{1,2} .* \$? -?\d?,?\d+\.\d{2})|(INTEREST CHARGE ON PURCHASES \$? -?\d?,?\d+\.\d{2})"
-    DATE_REGEX = re.compile(r"Close Date: (\w{3} \d{1,2}, \d{4})")
     text = get_all_statement_text(folder)
     matches = []
     for stmt in text:
         # extract text, then month and year from text per pdf
-        _, _, month, _, year = re.search(DATE_REGEX, stmt).group().split()
+        month, year = re.search(DATE_REG, stmt).groups()
+        day = str(monthrange(int(year), MONTH_NUM[month])[1])
 
-        for match in re.finditer(REG, stmt):
-            # print(
-            #     f"1 - {str(match[0]): <65} 2 - {str(match[1]): <65} 3 - {str(match[2]): <65}"
-            # )
-            if match[1]:  # matches a regular transaction
-                matches.append(" ".join([year, match[1]]))
-            else:  # matches interest charge
-                day = str(monthrange(int(year), MONTHS[month])[1])
-                matches.append(" ".join([year, month, day, match[2]]))
+        for match in re.findall(REG, stmt):
+            if match.upper().startswith("INTEREST CHARGE"):  # matches interest charge
+                matches.append(" ".join([year, month, day, match]))
+            else:
+                matches.append(" ".join([year, match]))
+
+    return matches
+
+
+def process_new_discover_statement(folder: str) -> list[str]:
+    DATE_REG = re.compile(r"(?<=AS OF [\d/]*\s*-)(\d{2})/\d{2}/(\d{4})")
+    REG = re.compile(
+        r"\d{2}\/\d{2}[\s\w\-\/\*#\\n'\.]* ?\$\d+\.\d{2}|TOTAL INTEREST FOR THIS PERIOD ?\$\d+\.\d{2}"
+    )
+
+    text = get_all_statement_text(folder)
+    matches = []
+    for stmt in text:
+        # extract text, then month and year from text per pdf
+        month, year = re.search(DATE_REG, stmt).groups()
+        day = str(monthrange(int(year), int(month))[1])
+        month = NUM_MONTH[int(month)]
+
+        for match in re.findall(REG, stmt):
+            if not match.upper().startswith("TOTAL INTEREST"):  # interest charge
+                day = match[3:5]
+                match = match[6:]
+            matches.append(" ".join([year, month, day, match]))
 
     return matches
 
@@ -83,5 +80,6 @@ def process_old_discover_transactions(folder: str) -> list[str]:
 if __name__ == "__main__":
     OLD = r"C:\Users\Chris\OneDrive\Python_Projects\Money-Manager\data\01_raw\pdfs\Discover\01_Old_Disc_pdfs"
     NEW = r"C:\Users\Chris\OneDrive\Python_Projects\Money-Manager\data\01_raw\pdfs\Discover\02_New_Disc_pdfs"
-    old_transactions = process_old_discover_transactions(OLD)
-    print(old_transactions)
+    TESTER = r"C:\Users\Chris\OneDrive\Python_Projects\Money-Manager\data\01_raw\pdfs\Discover\tester"
+    [print(x) for x in process_old_discover_statements(OLD)]
+    [print(x) for x in process_new_discover_statement(NEW)]
